8f342574a97f96a8ee43074fd85e282d82d06614
Christopher Roche <christopherroche@stmarksschool.org>
2018-05-09 09:38:51 -0400

    all changes on May 9

--- js/fan.js
+++ js/fan.js
@@ -3,17 +3,54 @@
 //alert("Hi its friday!!!");
 
 
+var numOfPhotos = 13;
+var currentPic = 0;
+
 
 function hidePhotos() {
 
 	// when you  add pictures change the number
-	var numOfPhotos = 13;
+	
 
 	for(var p = 1; p < numOfPhotos; p++)
 	{
 		var photoID = "image" + p;
 		document.getElementById(photoID).style.display = "none";
 	}
+}
+
+
+function doRightClick(){
+
+ 	// Step1 -- Hide Current Photo
+ 	var photoID = "image" + currentPic;
+ 	document.getElementById(photoID).style.display = "none";
+ 	// Step2 --Go To next Photo
+ 	currentPic--;
+
+ 	if(currentPic < 0)
+ 	{
+ 		currentPic= numOfPhotos-1;
+ 	}
+ 	// Step 3 show new photo
+ 	photoID = "image" + currentPic;
+ 	document.getElementById(photoID).style.display = "block";
+}
+
+function doLeftClick(){
+ 	//alert("You clicked the Left Arrow");
+ 	// Step1 -- Hide Current Photo
+ 	var photoID = "image" + currentPic;
+ 	document.getElementById(photoID).style.display = "none";
+ 	// Step2 --Go To next Photo
+ 	currentPic++;
+ 	if(currentPic >= numOfPhotos)
+ 	{
+ 		currentPic=0;
+ 	}
+ 	// Step 3 show new photo
+ 	photoID = "image" + currentPic;
+ 	document.getElementById(photoID).style.display = "block";
 }
 
 
